1fb92db6148181a08e9dd7bbd8aee05655c075c0
Bhavtosh Dwivedi <bhavtoshdwivedi79@gmail.com>
2026-06-14 17:39:07 +0530

    Add files via upload

---
+++ JS Faulty Calculator/script.js
@@ -0,0 +1,22 @@
+let random = Math.random();
+
+let a = prompt("Enter first Number");
+let c = prompt("Enter operation");
+let b = prompt("Enter second Number");
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**"
+}
+
+if (random > 0.1) {
+    //Perform correct calculation
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+}
+else {
+    //Perform wrong calculation
+    c = obj[c];
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+}
